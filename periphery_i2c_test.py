729dc3b700801ec66db90db8380b93a7e7bb810d
bob <rejarder@hotmail.com>
2023-05-07 22:22:36 -0400

    sample code from periphery docs

---
+++ periphery_i2c_test.py
@@ -0,0 +1,11 @@
+from periphery import I2C
+
+# Open i2c-0 controller
+i2c = I2C("/dev/i2c-0")
+
+# Read byte at address 0x100 of EEPROM at 0x50
+msgs = [I2C.Message([0x01, 0x00]), I2C.Message([0x00], read=True)]
+i2c.transfer(0x50, msgs)
+print("0x100: 0x{:02x}".format(msgs[1].data[0]))
+
+i2c.close()
